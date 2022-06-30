762da59ade8acbbc212a45a2645a98a4f82d92a0
Jack <jack@CHD.com>
2022-06-30 15:39:44 +0800

    hot-fix commit1

--- src/main/java/com/atguigu/test/TestGit.java
+++ src/main/java/com/atguigu/test/TestGit.java
@@ -11,5 +11,6 @@ public class TestGit {
         System.out.println("hello git");
         System.out.println("hello git");
         System.out.println("hello git1");
+        System.out.println("hello git2");
     }
 }
