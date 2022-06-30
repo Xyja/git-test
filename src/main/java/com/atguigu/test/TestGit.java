ca81af20b3366ef2477373151d63e7762e07b5f5
Jack <jack@CHD.com>
2022-06-30 15:35:52 +0800

    first commit

---
+++ src/main/java/com/atguigu/test/TestGit.java
@@ -0,0 +1,14 @@
+package com.atguigu.test;
+
+/**
+ * @author Yongjiu, X
+ * @create 2022-06-30 15:30
+ */
+public class TestGit {
+
+    public static void main(String[] args) {
+        System.out.println("hello git");
+        System.out.println("hello git");
+        System.out.println("hello git");
+    }
+}
